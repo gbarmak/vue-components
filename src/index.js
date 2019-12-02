488ec1a6b4c170cb475a861220da872f45e1130d
Gary Barmak <gary.barmak@S05200-MBPR.local>
2019-12-02 11:55:28 -0600

    Publish vue-components as library to be consumed by other apps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export { default as HelloWorld } from './components/HelloWorld.vue'
+// export { default as HelloWorld } from './components/HelloWorld.vue'
+// export const foo = 'hello world'
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const Components = {
+  HelloWorld
+}
+
+Object.keys(Components).forEach((name) => {
+  Vue.component(name, Components[name])
+})
+
+export default Components
